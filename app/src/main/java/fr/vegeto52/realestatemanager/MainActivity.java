454c7d5b5b59e547e516c5eae659d5fc83f00634
Vegeto52k <kekemath@hotmail.fr>
2023-11-03 17:28:21 +0100

    Bug fixes

---
+++ app/src/main/java/fr/vegeto52/realestatemanager/MainActivity.java
@@ -0,0 +1,37 @@
+package fr.vegeto52.realestatemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewMain;
+    private TextView textViewQuantity;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.textViewMain = findViewById(R.id.activity_main_activity_text_view_quantity);
+        this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
+
+        this.configureTextViewMain();
+        this.configureTextViewQuantity();
+    }
+
+    private void configureTextViewMain(){
+        this.textViewMain.setTextSize(15);
+        this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
+    }
+
+    private void configureTextViewQuantity(){
+        int quantity = Utils.convertDollarToEuro(100);
+        this.textViewQuantity.setTextSize(20);
+        this.textViewQuantity.setText(String.valueOf(quantity));
+    }
+}
